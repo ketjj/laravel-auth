b49025cfa62029ee4c9a9cd8434b539fa3bf6cfe
ketjj <jorjolianiket@gmail.com>
2022-07-05 17:21:11 +0200

    adding 13-14

---
+++ database/seeds/PostsTableSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Post;
+use Faker\Generator as Faker;
+
+
+class PostsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(Faker $faker)
+    {
+
+        for($i = 0; $i < 40; $i++){
+            $new_post = new Post();
+            $new_post->title = 'Post for Today';
+            $new_post->slug = Post::SlugGen($new_post->title );
+            $new_post->content = $faker->text(200);
+            $new_post->save();
+        }
+    }
+}
